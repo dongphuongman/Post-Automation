643d8a0efc9291a77e27231b6ea14e5382f5e5c0
Dong Man <dongman@Dongs-MacBook-Air.local>
2026-07-30 13:31:34 +0700

    Update bot config: rename page to Linkosys, add page URL, clean description

--- bot/lib/config.js
+++ bot/lib/config.js
@@ -1,10 +1,11 @@
-const PAGE_NAME = 'Dinh Phuong';
+const PAGE_NAME = 'Linkosys';
 
 const GROUPS = [
   'https://www.facebook.com/groups/comailo',
   'https://www.facebook.com/groups/groupaivietnam/',
   'https://www.facebook.com/groups/openclawxvn/',
   'https://www.facebook.com/groups/861108920047086/',
+  'https://web.facebook.com/Linkosys.It/'
 ];
 
 const FB_API_VERSION = 'v21.0';
